819e978ba551a3c88d4c9663a5094e6c48824858
Vishal-257 <112974481+Vishal-257@users.noreply.github.com>
2023-10-01 14:43:08 +0530

    Project_more updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import './index.css'
-import About from './components/About'
-import Navbar from './components/Navbar'
-import { Routes, Route } from 'react-router-dom'
-import Footer from './components/Footer'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar/>
-      <Routes>
-        <Route path='/' exact element={<About/>}/>
-      </Routes>
-      <Footer/>
+      
     </>
   )
 }
